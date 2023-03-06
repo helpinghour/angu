f7700304b05eeec9e3b4ca98676635eaee56b0b7
unknown <helpinghour@gmail.com>
2023-03-06 15:41:44 +0500

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { HomeComponent } from './pages/home/home.component';
 import { StoresComponent } from './pages/stores/stores.component';
-
 
 const routes: Routes = [
   { path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,8 +15,8 @@ import { FooterComponent } from './include/footer/footer.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    StoresComponent
+    // HomeComponent,
+    // StoresComponent
   ],
   imports: [
     BrowserModule,
